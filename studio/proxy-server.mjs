80b6dff06b056c5ffa7f02e605f5198498205099
Antonio Ramirez <peperamirez89@gmail.com>
2026-03-27 14:19:55 -0700

    feat(studio): make proxy-server port configurable via PORT env var

--- studio/proxy-server.mjs
+++ studio/proxy-server.mjs
@@ -37,11 +37,11 @@ if (keyPath && certPath) {
 
     server = createHttpsServer(httpsOptions, requestHandler);
     serverProtocol = 'https';
-    serverPort = 8443; // Use 8443 for HTTPS
+    serverPort = process.env.PORT || 8443; // Use 8443 for HTTPS
 } else {
     server = createHttpServer(requestHandler);
     serverProtocol = 'http';
-    serverPort = 8080; // Use 8080 for HTTP
+    serverPort = process.env.PORT || 8080; // Use 8080 for HTTP
 }
 
 // Common request handler
