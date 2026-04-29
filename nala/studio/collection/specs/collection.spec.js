71d14f597fcf4c4169edeb1af95d217dc3142e6a
auto <auto@sj1010122072230.corp.adobe.com>
2026-04-28 19:14:22 -0700

    test(studio): replace TODO placeholders with real fragment IDs in collection spec

--- nala/studio/collection/specs/collection.spec.js
+++ nala/studio/collection/specs/collection.spec.js
@@ -6,7 +6,7 @@ export default {
             name: '@studio-collection-add-card-button-visible',
             path: '/studio.html',
             data: {
-                cardid: 'TODO-replace-with-actual-collection-fragment-id',
+                cardid: '198543ee-2ace-48be-be60-f3e7cc775608',
             },
             browserParams: '#page=fragment-editor&path=nala&fragmentId=',
             tags: '@mas-studio @collection @collection-add-card @collection-smoke',
@@ -16,7 +16,7 @@ export default {
             name: '@studio-collection-add-card-input-expand',
             path: '/studio.html',
             data: {
-                cardid: 'TODO-replace-with-actual-collection-fragment-id',
+                cardid: '198543ee-2ace-48be-be60-f3e7cc775608',
             },
             browserParams: '#page=fragment-editor&path=nala&fragmentId=',
             tags: '@mas-studio @collection @collection-add-card',
@@ -26,9 +26,9 @@ export default {
             name: '@studio-collection-add-card-paste-link',
             path: '/studio.html',
             data: {
-                cardid: 'TODO-replace-with-actual-collection-fragment-id',
+                cardid: '198543ee-2ace-48be-be60-f3e7cc775608',
                 cardLink:
-                    'TODO-replace-with-actual-card-link-https://mas.adobe.com/studio.html#page=content&path=nala&query=<id>',
+                    'https://mas.adobe.com/studio.html#page=content&path=nala&query=48a759ce-3c9a-4158-9bc3-b21ffa07e8e4',
                 successToast: 'Card added',
             },
             browserParams: '#page=fragment-editor&path=nala&fragmentId=',
@@ -39,8 +39,9 @@ export default {
             name: '@studio-collection-add-card-duplicate',
             path: '/studio.html',
             data: {
-                cardid: 'TODO-replace-with-actual-collection-fragment-id',
-                duplicateCardLink: 'TODO-replace-with-link-to-card-already-in-collection',
+                cardid: '198543ee-2ace-48be-be60-f3e7cc775608',
+                duplicateCardLink:
+                    'https://mas.adobe.com/studio.html#page=content&path=nala&query=f7fdf15d-bcb0-40c4-9a8f-fa103fc640e7',
                 duplicateError: 'Card already in collection',
             },
             browserParams: '#page=fragment-editor&path=nala&fragmentId=',
@@ -51,7 +52,7 @@ export default {
             name: '@studio-collection-add-card-invalid-link',
             path: '/studio.html',
             data: {
-                cardid: 'TODO-replace-with-actual-collection-fragment-id',
+                cardid: '198543ee-2ace-48be-be60-f3e7cc775608',
                 invalidValue: 'not-a-card-link',
                 invalidError: 'Paste a valid card link',
             },
@@ -63,7 +64,7 @@ export default {
             name: '@studio-collection-add-card-blur-collapses-input',
             path: '/studio.html',
             data: {
-                cardid: 'TODO-replace-with-actual-collection-fragment-id',
+                cardid: '198543ee-2ace-48be-be60-f3e7cc775608',
             },
             browserParams: '#page=fragment-editor&path=nala&fragmentId=',
             tags: '@mas-studio @collection @collection-add-card',
